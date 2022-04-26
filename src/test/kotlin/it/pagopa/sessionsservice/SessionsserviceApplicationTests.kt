5cfba3b69a045c44d12dcda24f7a36a6a09637d1
MarcoAGreco <marcoandreagreco@gmail.com>
2022-04-26 16:02:36 +0200

    [CHK-342] setup project strucutre

---
+++ src/test/kotlin/it/pagopa/sessionsservice/SessionsserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.pagopa.sessionsservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SessionsserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
