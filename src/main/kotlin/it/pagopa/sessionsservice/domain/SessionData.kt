075441306bc47f0f7f05117433958fcf6e3bbc60
MarcoAGreco <marcoandreagreco@gmail.com>
2022-05-03 17:27:24 +0200

    fix: minor fixes

--- src/main/kotlin/it/pagopa/sessionsservice/domain/SessionData.kt
+++ src/main/kotlin/it/pagopa/sessionsservice/domain/SessionData.kt
@@ -2,15 +2,15 @@ package it.pagopa.sessionsservice.domain
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
+import java.util.regex.Pattern
 
 @RedisHash("SessionData")
 data class SessionData(@Id var rptId: RptId, var email: String, var paymentToken: String, var sessionToken: String?){
+    companion object {
+        private val emailRegex = "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])".toRegex()
+    }
     init {
-        val emailRegex = "(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])".toRegex()
-        // val paymentTokenRegex = "".toRegex()
-
         require(email matches emailRegex) { "Email must be RFC 5322 compliant." }
-        // require(paymentToken matches paymentTokenRegex)  { "Name is blank" }
     }
 }
 
