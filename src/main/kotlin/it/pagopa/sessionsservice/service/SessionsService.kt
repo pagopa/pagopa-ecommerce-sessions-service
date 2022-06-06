5b97c945e6ca054108447cb86dac25b9693e8327
Giovanni Berti <giovanni.berti@pagopa.it>
2022-06-06 15:15:45 +0200

    [CHK-451] Migrate reactive code to Kotlin coroutines (#13)
    * refactor(misc): misc ide suggestions
    
    * refactor(service): begin transition to coroutine-based reactive operators
    
    * refactor(coroutines): switch to `kotlin-spring` openapi generator
    
    * fix(compile): add missing directives to kotlin+spring maven plugins

--- src/main/kotlin/it/pagopa/sessionsservice/service/SessionsService.kt
+++ src/main/kotlin/it/pagopa/sessionsservice/service/SessionsService.kt
@@ -3,13 +3,15 @@ package it.pagopa.sessionsservice.service
 import it.pagopa.generated.session.server.model.SessionDataDto
 import it.pagopa.sessionsservice.domain.SessionData
 import it.pagopa.sessionsservice.session.JwtTokenUtil
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactor.awaitSingle
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.redis.core.ReactiveRedisOperations
 import org.springframework.stereotype.Service
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 
 
 @Service
@@ -20,47 +22,50 @@ class SessionsService {
     private lateinit var jwtTokenUtil: JwtTokenUtil
     var logger: Logger = LoggerFactory.getLogger(SessionsService::class.java)
 
-    fun validateSession(sessionData: SessionDataDto): Mono<Boolean> {
-        return getToken(sessionData.rptId).map { data ->
-            if (data == null || data.sessionToken.isNullOrEmpty()){
-                logger.info("Session data validation failed. Session data not found in db.")
-                return@map false
-            } else {
-                val isValid = jwtTokenUtil.validateToken(data.sessionToken!!)
-                logger.info("Session data validation - success: ${isValid}.")
+    suspend fun validateSession(sessionData: SessionDataDto): Boolean {
+        val data = getToken(sessionData.rptId)
+        return if (data == null || data.sessionToken.isNullOrEmpty()) {
+            logger.info("Session data validation failed. Session data not found in db.")
+            false
+        } else {
+            val isValid = jwtTokenUtil.validateToken(data.sessionToken!!)
+            logger.info("Session data validation - success: ${isValid}.")
 
-                return@map data.sessionToken == sessionData.sessionToken && isValid
-            }
+            data.sessionToken == sessionData.sessionToken && isValid
         }
     }
 
-    fun getToken(rptId: String): Mono<SessionData> {
+    suspend fun getToken(rptId: String): SessionData? {
         logger.info("Searching for session data with rptId (${rptId}.")
-        return sessionOps.opsForValue().get(rptId)
+        return sessionOps.opsForValue().get(rptId).awaitSingle()
     }
 
-    fun getAllTokens(): Flux<SessionDataDto> {
+    fun getAllTokens(): Flow<SessionDataDto> {
         logger.info("Searching for all session data.")
-        return Flux.concat(sessionOps.scan().map(sessionOps.opsForValue()::get)).map {
-            SessionDataDto()
-                .sessionToken(it.sessionToken)
-                .paymentToken(it.paymentToken)
-                .email(it.email)
-                .rptId(it.rptId)
+        return sessionOps.scan().asFlow().map {
+            return@map sessionOps.opsForValue().get(it).awaitSingle()
+        }.map {
+            SessionDataDto(
+                sessionToken = it.sessionToken!!,
+                paymentToken = it.paymentToken,
+                email = it.email,
+                rptId = it.rptId,
+            )
         }
     }
 
-    fun postToken(sessionDataDto: SessionDataDto): Mono<SessionData> {
+    suspend fun postToken(sessionDataDto: SessionDataDto): SessionData? {
         logger.info("Creating new session data for ${sessionDataDto.rptId}.")
 
         val sessionData = SessionData(
-            sessionDataDto.rptId, sessionDataDto.email, sessionDataDto.paymentToken, jwtTokenUtil?.generateToken(sessionDataDto)
+            sessionDataDto.rptId, sessionDataDto.email, sessionDataDto.paymentToken, jwtTokenUtil.generateToken(sessionDataDto)
         )
-        return sessionOps.opsForValue().set(sessionDataDto.rptId, sessionData)
-            .map {
-                if(it){
-                    return@map sessionData
-                } else return@map null
-            }
+        val success = sessionOps.opsForValue().set(sessionDataDto.rptId, sessionData).awaitSingle()
+
+        return if (success) {
+            sessionData
+        } else {
+            null
+        }
     }
 }
